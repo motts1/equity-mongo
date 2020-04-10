c8384a5a5d250cdd9706af0b6199113e2f153582
user <user@users-MacBook-Pro.local>
2020-04-10 06:35:29 -0700

    mongo update

---
+++ serverless-mongo-master/models/User.js
@@ -0,0 +1,24 @@
+import { Schema, model } from 'mongoose';
+const UserSchema = new Schema({
+    id: String,
+    artistId: String,
+    userName: String,
+    // artistName: String,
+    // title: String,
+    // type: String,
+    // bio: String,
+    // location: String,
+    // followers: Number,
+    // plays: Number,
+    // trackCount: Number,
+    // soundcloud: String,
+    // subscribers: Number,
+    // artist: String,
+    // price: Number,
+    // genre: String,
+    // image: String,
+    // backImage: String,
+    // link1: String,
+    // link2: String,
+});
+export default model('User', UserSchema);
---
+++ serverless-mongo-master/users.js
@@ -0,0 +1,156 @@
+'use strict';
+const User = require('./models/User').default;
+const mongoose = require('mongoose');
+
+module.exports.create = async (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+  try {
+    await mongoose.connect(process.env.DB);
+    const user = await User.create(JSON.parse(event.body));
+    await mongoose.connection.close()
+    return callback(null, {
+      statusCode: 200,
+      body: JSON.stringify(user),
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true
+      }
+    });
+  }
+  catch (err) {
+    await mongoose.connection.close()
+    return callback(null, {
+      statusCode: err.statusCode || 500,
+      body: JSON.stringify(err),
+      headers: {
+        'Content-Type': 'text/plain',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true
+      }
+    });
+  }
+};
+
+module.exports.getOne = async (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+  console.log('User Event: ', event);
+  console.log('User Context: ', context);
+  console.log('User Callback: ', callback);
+  try {
+    await mongoose.connect(process.env.DB)
+    const user = await User.find({ id: event.pathParameters.id });
+    await mongoose.connection.close();
+    console.log('User object: ', user);
+    callback(null, {
+      statusCode: 200,
+      body: JSON.stringify(user),
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true
+      }
+    });
+  }
+  catch (err) {
+    console.log('User error: ', err);
+    await mongoose.connection.close()
+    callback(null, {
+      statusCode: err.statusCode || 500,
+      body: JSON.stringify(err),
+      headers: {
+        'Content-Type': 'text/plain',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true
+      }
+    });
+  }
+};
+
+module.exports.getAll = async (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+  try {
+    await mongoose.connect(process.env.DB);
+    const users = await User.find();
+    await mongoose.connection.close()
+    callback(null, {
+      statusCode: 200,
+      body: JSON.stringify(users),
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true
+      }
+    })
+  } catch(err) {
+    console.log('Error: ', err)
+    await mongoose.connection.close()
+    callback(null, {
+      statusCode: err.statusCode || 500,
+      body: JSON.stringify(err),
+      headers: {
+        'Content-Type': 'text/plain',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true
+      }
+    })
+  }
+};
+
+module.exports.update = async (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+  try {
+    await mongoose.connect(process.env.DB)
+    const user = await User.findOneAndUpdate({ id: event.pathParameters.id }, JSON.parse(event.body), { new: true });
+    await mongoose.connection.close();
+    return callback(null, {
+      statusCode: 200,
+      body: JSON.stringify(user),
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true
+      }
+    });
+  }
+  catch (err) {
+    await mongoose.connection.close()
+    return callback(null, {
+      statusCode: err.statusCode || 500,
+      body: JSON.stringify(err),
+      headers: {
+        'Content-Type': 'text/plain',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true
+      }
+    });
+  }
+};
+
+module.exports.delete = async (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+  try {
+    await mongoose.connect(process.env.DB)
+    const user = await User.findOneAndRemove({ id: event.pathParameters.id });
+    await mongoose.connection.close();
+    return callback(null, {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: 'Removed user with id: ' + user._id,
+        user: user
+      }),
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true
+      }
+    });
+  }
+  catch (err) {
+    await mongoose.connection.close()
+    return callback(null, {
+      statusCode: err.statusCode || 500,
+      body: JSON.stringify(err),
+      headers: {
+        'Content-Type': 'text/plain',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true
+      }
+    });
+  }
+};
